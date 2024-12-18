5e4e7d6ffb8e2972e42744604d303e71446a817c
vsltech <78464025+innovatiostech@users.noreply.github.com>
2024-12-18 14:21:17 +0530

    v1.0

---
+++ benchmarkdata-dashboard/views.py
@@ -0,0 +1,67 @@
+data_list = [
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            ['225388','imerfrfn_url','',,,....]
+            
+            ]
+
+for data in data_list:
+    <div class="analytics-group1000005164">
+          <div class="analytics-checkbox4">
+            <img src="public/external/rectangle5912i129-heoo-200h.png" alt="Rectangle5912I129" class="analytics-rectangle59122">
+          </div>
+          <span class="analytics-text43">{data[0]}</span>
+          <span class="analytics-text44">{data[1]}</span>
+          <span class="analytics-text45">{data[2]}</span>
+          <span class="analytics-text46">
+            Benchmark: PPE Non-Compliance(no_vest)
+          </span>
+          <span class="analytics-text47">NYE</span>
+          <img src="public/external/screenshot202410281347411291-ptlo-200h.png" alt="Screenshot202410281347411291" class="analytics-screenshot202410281347414">
+          <img src="public/external/letsiconscheckfill1291-6gp.svg" alt="letsiconscheckfill1291" class="analytics-letsiconscheckfill4">
+          <div class="analytics-group10000051594">
+            <div class="analytics-group10000050654">
+              <div class="analytics-group100000506122">
+                <div class="analytics-companies-facebook22"></div>
+              </div>
+              <img src="public/external/lsiconviewfilled1291-t404.svg" alt="lsiconviewfilled1291" class="analytics-lsiconviewfilled4">
+            </div>
+            <div class="analytics-group10000051564">
+              <div class="analytics-group100000506123">
+                <div class="analytics-companies-facebook23"></div>
+              </div>
+              <div class="analytics-mingcuteaddfill4">
+                <div class="analytics-group18">
+                  <img src="public/external/vector1291-l8d.svg" alt="Vector1291" class="analytics-vector21">
+                </div>
+              </div>
+            </div>
+            <div class="analytics-group10000051574">
+              <div class="analytics-group100000506124">
+                <div class="analytics-companies-facebook24"></div>
+              </div>
+              <div class="analytics-mingcutecheckfill4">
+                <div class="analytics-group19">
+                  <img src="public/external/vector1291-6n3r.svg" alt="Vector1291" class="analytics-vector22">
+                </div>
+              </div>
+            </div>
+            <div class="analytics-group10000051584">
+              <div class="analytics-group100000506125">
+                <div class="analytics-companies-facebook25"></div>
+              </div>
+              <img src="public/external/iconoircancel1291-aw8g.svg" alt="iconoircancel1291" class="analytics-iconoircancel4">
+            </div>
+          </div>
+        </div>
